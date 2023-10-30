4c7b47a050831f86562ff714007e61a79128ae79
Samuel Cavalcante <65915755+samuucavalcante@users.noreply.github.com>
2023-10-30 14:40:43 -0300

    feat
    create about me page

---
+++ src/components/Main/AboutMe/index.tsx
@@ -0,0 +1,17 @@
+export function AboutMe() {
+  return (
+    <div className="flex justify-center items-center flex-col my-4 ">
+      <div className="w-full max-w-4xl flex justify-center items-center flex-col gap-2">
+        <h2 className="text-3xl font-bold">Sobre Mim</h2>
+        <p className="text-md text-center text-neutral-500 font-semibold  ">
+          Desenvolvedor Front-end com experiência em React.js, Next.js, Redux,
+          JavaScript, TypeScript, Styled Components, Material UI e consumo de
+          APIs REST. Apaixonado pelo desenvolvimento de componentes
+          reutilizáveis. Também possui experiência em projetos gerenciados por
+          Metodologias Ágeis. Formado em Sistemas de Informação e cursando
+          Especialização em Front-End na Digital House Brasil.
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,11 @@
+import { AboutMe } from './AboutMe'
 import Apresentation from './Apresentation'
 
 export function Main() {
   return (
     <>
       <Apresentation />
+      <AboutMe />
     </>
   )
 }
